934688cfd238d9144090fd2c63c15632fe3dcced
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-22 21:50:30 +0800

    AM 01st commit

---
+++ SpringbootBasic01Application.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasic01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBasic01Application.class, args);
+    }
+
+}
